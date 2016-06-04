dd690687d49c70ba9e2b1c2d8c5098477c0aa3f7
Worm4047 <abhidagar@rocketmail.com>
2016-06-04 22:23:48 +0530

    first

--- index.py
+++ index.py
@@ -22,30 +22,6 @@ decision_tree_classifier = DecisionTreeClassifier(class_weight=None, criterion='
             presort=False, random_state=None, splitter='best')
 decision_tree_classifier.fit(features_train,labels_train)
 
-##TUNING THE CLASSIFIER######
-"""
-from sklearn.cross_validation import KFold
-
-kf = KFold(len(features), n_folds=10)
-
-from sklearn.grid_search import GridSearchCV
-
-
-parameter_grid = {'criterion': ['gini', 'entropy'],
-                  'splitter': ['best', 'random'],
-                  'max_depth': [1, 2, 3, 4, 5],
-                  'max_features': [1, 2, 3, 4]}
-
-grid_search = GridSearchCV(clf,
-                           param_grid=parameter_grid,
-                           cv=kf)
-
-grid_search.fit(features, labels)
-#print('Best score: {}'.format(grid_search.best_score_))
-#print('Best parameters: {}'.format(grid_search.best_params_))
-print grid_search.best_estimator_
-"""
-
 
 from sklearn import cross_validation
 
@@ -54,22 +30,6 @@ print 'rf_scores : '+' , '.join(rf_scores)
 
 
 
-#Taking testing set from cleaned_test.csv
-"""
-test_set = pd.read_csv('cleaned_test.csv')
-test_set=test_set.drop('Unnamed: 0',1)
-test_set=test_set.drop('shot_made_flag',1)
-pred=decision_tree_classifier.predict(test_set)
-main_text=''
 
-test_set = pd.read_csv('predicting_set.csv')
-test_set=test_set.drop('Unnamed: 0',1)
-test_set=test_set.drop('shot_made_flag',1)
-dic={'shot_id':[],'shot_made_flag':[]}
-text='shot_id,shot_made_flag'+'\n'
-for i in range(len(test_set)):
-	text += str(test_set['shot_id'][i])+','+str(pred[i])+'\n'
 
-f = open('submission.csv','w')
-f.write(text)
-"""
+
